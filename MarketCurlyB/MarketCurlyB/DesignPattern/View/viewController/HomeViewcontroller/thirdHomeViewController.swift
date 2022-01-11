883c055841a7e048ce4e12a7ad2b6250a44175d6
visionjuyoung <visionjyk21@naver.com>
2022-01-11 19:27:56 +0900

    홈-베스트 페이지 구성

--- MarketCurlyB/MarketCurlyB/DesignPattern/View/viewController/HomeViewcontroller/thirdHomeViewController.swift
+++ MarketCurlyB/MarketCurlyB/DesignPattern/View/viewController/HomeViewcontroller/thirdHomeViewController.swift
@@ -9,9 +9,64 @@ import UIKit
 
 class thirdHomeViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setCollectionView()
     }
+    
+    func setCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+extension  thirdHomeViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bestProductCell", for: indexPath) as? bestProductCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemspacing: CGFloat = 10
+        
+        let width: CGFloat = (collectionView.bounds.width - itemspacing - 40)/2
+        let height: CGFloat = width * 320/160
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+            case UICollectionView.elementKindSectionHeader:
+                let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "bestProductHeaderCell", for: indexPath)
+                return headerView
+            default:
+                assert(false)
+        }
+    }
+}
+
+class bestProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var markLabel: UILabel!
+    
+}
+
+class bestProductHeaderCell: UICollectionReusableView {
+    
+    @IBOutlet weak var filterLabel: UILabel!
+    @IBOutlet weak var chevornImg: UIImageView!
+    
 }
